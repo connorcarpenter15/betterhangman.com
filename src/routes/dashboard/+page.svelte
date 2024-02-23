bd902cf3bd3859106ed1e78a71a883e319e2b1de
Connor <connorcarpenter15@gmail.com>
2024-02-23 09:41:52 -0500

    add auth

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import AuthReset from "../../components/AuthReset.svelte";
+  import { authHandlers, authStore } from "../../stores/authStore";
+
+  let email;
+  authStore.subscribe((curr) => {
+    console.log("CURR", curr);
+    email = curr?.currentUser?.email;
+  });
+</script>
+
+{#if $authStore.currentUser}
+  <div class="align-center flex flex-1 flex-col justify-center">
+    <h1 class="align-center">CURRENT USER: {email}</h1>
+    <AuthReset />
+    <button on:click={authHandlers.logout}>Logout</button>
+  </div>
+{:else}
+  <div>Loading....</div>
+{/if}
