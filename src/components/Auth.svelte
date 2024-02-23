0b6050313d847985301196c9f9a3200c3e3fc8db
Connor <connorcarpenter15@gmail.com>
2024-02-23 18:27:46 -0500

    add login page features

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -5,21 +5,30 @@
   let email = "";
   let password = "";
   let confirmPassword = "";
+  let errorText = "";
 
   async function handleSubmit() {
     if (!email || !password || (register && !confirmPassword)) {
+      errorText = "Please fill missing fields";
+      return;
+    }
+
+    if (register && password !== confirmPassword) {
+      errorText = "Passwords do not match";
       return;
     }
 
     if (register && password === confirmPassword) {
       try {
         await authHandlers.signup(email, password);
+        errorText = "";
       } catch (err) {
         console.log(err);
       }
     } else {
       try {
         await authHandlers.login(email, password);
+        errorText = "";
       } catch (err) {
         console.log(err);
       }
@@ -28,59 +37,104 @@
       window.location.href = "/dashboard";
     }
   }
+
+  let showConfirmPassword = false;
+  let showPassword = false;
+
+  function onInput(event, field) {
+    if (field === "password") {
+      password = event.target.value;
+    } else if (field === "confirmPassword") {
+      confirmPassword = event.target.value;
+    }
+  }
+
+  function switchRegister() {
+    register = !register;
+    errorText = "";
+    email = "";
+    password = "";
+    confirmPassword = "";
+  }
 </script>
 
-<div class="align-center m-auto flex h-screen w-max flex-1 flex-col">
+<div class="align-center m-auto flex h-screen flex-1 flex-col">
   <h1 class="m-auto mb-0 text-3xl font-medium">
-    {register ? "Register" : "Log In"}
+    {register ? "Register" : "Welcome Back"}
   </h1>
+
   <form class="flex flex-col">
-    <label class="m-auto mb-1 mt-8 rounded border border-black p-1 px-2">
+    <label class="m-auto mb-1 mt-8 w-72 rounded border border-black p-1 px-2">
       <input bind:value={email} type="email" placeholder="Email" />
     </label>
-    <label class="m-auto mb-1 mt-1 rounded border border-black p-1 px-2">
-      <input bind:value={password} type="password" placeholder="Password" />
+
+    <label
+      class="align-center m-auto mb-1 mt-1 flex w-72 flex-1 justify-between rounded border border-black p-1 px-2"
+    >
+      <input
+        value={password}
+        type={showPassword ? "text" : "password"}
+        placeholder="Password"
+        on:input={(e) => onInput(e, "password")}
+      />
+
+      <button type="button" on:click={() => (showPassword = !showPassword)}
+        >{showPassword ? "Hide" : "Show"}</button
+      >
     </label>
+
     {#if register}
-      <label class="m-auto mb-2 mt-1 rounded border border-black p-1 px-2">
+      <label
+        class="align-center m-auto mb-2 mt-1 flex w-72 flex-1 justify-between rounded border border-black p-1 px-2"
+      >
         <input
-          bind:value={confirmPassword}
-          type="password"
+          value={confirmPassword}
+          type={showConfirmPassword ? "text" : "password"}
           placeholder="Confirm Password"
+          on:input={(e) => onInput(e, "confirmPassword")}
         />
+
+        <button
+          type="button"
+          on:click={() => (showConfirmPassword = !showConfirmPassword)}
+          >{showConfirmPassword ? "Hide" : "Show"}</button
+        >
       </label>
     {/if}
+
+    {#if errorText}
+      <p class="align-center flex flex-1 justify-center text-red-500">
+        {errorText}
+      </p>
+    {/if}
+
     <button
       on:click={handleSubmit}
-      class="m-auto mt-8 rounded-lg bg-black p-8 pb-2 pt-2 text-white"
-      >Submit</button
+      class="m-auto mt-8 rounded-2xl bg-black p-8 pb-2 pt-2 text-white"
+      >{register ? "Sign Up" : "Log In"}</button
     >
   </form>
   {#if register}
     <div
-      on:click={() => {
-        register = false;
-      }}
+      on:click={switchRegister}
       on:keydown={() => {}}
       role="button"
       tabindex="0"
       class="m-auto mt-6"
     >
       Already have an account?
-      <p class="inline-block underline">Log in</p>
+      <p class="inline-block text-blue-400">Log in</p>
     </div>
   {:else}
     <div
-      on:click={() => {
-        register = true;
-      }}
+      on:click={switchRegister}
       on:keydown={() => {}}
       role="button"
       tabindex="0"
       class="m-auto mb-1 mt-6"
     >
       Don't have an account?
-      <p class="inline-block underline">Sign Up</p>
+      <p class="inline-block text-blue-400">Sign Up</p>
     </div>
     <div
       on:click={() => {
@@ -89,7 +143,7 @@
       on:keydown={() => {}}
       role="button"
       tabindex="0"
-      class="m-auto mt-1 underline"
+      class="m-auto mt-1 text-blue-400"
     >
       Forgot Password?
     </div>
